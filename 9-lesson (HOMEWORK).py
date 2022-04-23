9cf62765c521948e771a3a7e570244a0ca2df194
Dilyor1998 <104087595+Dilyor1998@users.noreply.github.com>
2022-04-24 01:08:49 +0500

    UYGA VAZIFA

---
+++ 9-lesson (HOMEWORK).py
@@ -0,0 +1,22 @@
+# # 9-Lesson (Homework)
+
+# ichimliklar = ['aroq','kola','piva','kanyak','vino']
+# for ichimlik in ichimliklar:
+#     print(f"{ichimlik} ichish zarar tashang\n")
+# print("yuqorida sikl 5 marta takrorlarndi")
+
+# toq_sonlar = range(11,100,2)
+# for toq in toq_sonlar:
+#     print(f"{toq ** 3} \n")
+
+# kinolar = []
+# print("5 ta eng sevimli kinoni kiriting")
+# for kino in range(5):
+#     kinolar.append(input(f"{kino+1} - kinoni nomi nima: "))
+# print(f"sizning sevimli konlaringiz\n {kinolar}\t")
+ 
+# nechta = int(input("bugun nechta odam bn suhbatlashdingiz "))
+# ismlar = []
+# for sum in range(nechta):
+#     ismlar.append(input(f"{sum+1} ning ismi kim edi "))
+# print(ismlar)
