a63892462ad1e457a33c78b4108fb120870d39bf
Dilyor1998 <104087595+Dilyor1998@users.noreply.github.com>
2022-04-24 00:31:26 +0500

    Create 9-lesson (FOR LOOP).py

---
+++ 9-lesson (FOR LOOP).py
@@ -0,0 +1,27 @@
+# # 9-Lesson FOR LOOP
+
+# mehmonlar = ['Ali','Aziz','shox','Dilyor','Otash']
+# for mehmon in mehmonlar:
+#     print("Salom",mehmon)
+
+# for mehmon in mehmonlar:
+#     print(f"Hurmatli {mehmon}, sizni ertaga soat 5 da PDP da kutamiz ") 
+#     print('Hurmat bilan Donyor\n')
+
+# sonlar = list(range(1,11)) # 1 dan 10 gacha bolgan sonlar yaratish 
+# for son in sonlar:# sonlar ichidagi har  bir sonni kvadrtaga chiqarish
+#     print(f"{son} ning kvadrati {son ** 2} ga teng")
+
+# sonlar = list(range(11)) # 0 dan 10 gacha bolgan sonlar 
+# sonlar_kvadrati = [] # bo'sh ro'yxat
+# for son in sonlar:
+#     sonlar_kvadrati.append(son ** 2) # uning kv.ni hisoblar toldirish
+    
+# print(sonlar)
+# print(sonlar_kvadrati)
+
+# cars = []
+# print("5 ta moshina nomini kiriting ")
+# for n in range(5):
+#     cars.append(input(f"{n+1}- moshinani kiriting "))
+# print(cars)
